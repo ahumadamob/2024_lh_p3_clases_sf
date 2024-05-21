9c1040a8a8997ccc055f2a32cf9944b7acba6dee
Mario Ahumada <ahumadamob@gmail.com>
2024-05-21 20:35:06 -0300

    Metodos POST, PUT y DELETE

--- src/main/java/imb/lh/p3/clases/controller/MascotaController.java
+++ src/main/java/imb/lh/p3/clases/controller/MascotaController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import imb.lh.p3.clases.entity.Mascota;
@@ -28,6 +32,22 @@ public class MascotaController {
 	public Mascota buscarMascotaPorId(@PathVariable("id") Long id) {
 		return service.getMascotasById(id);
 	}
+	
+	@PostMapping("/mascota")
+	public Mascota crearMascota(@RequestBody Mascota mascota){
+		return service.guardar(mascota);
+	}	
+	
+	@PutMapping("/mascota")
+	public Mascota actualizarMascota(@RequestBody Mascota mascota) {
+		return service.guardar(mascota);
+	}
+	
+	@DeleteMapping("/mascota/{id}")
+	public void eliminarMascota(@PathVariable("id") Long id){
+		service.eliminar(id);
+	}
+
 	
 
 }
